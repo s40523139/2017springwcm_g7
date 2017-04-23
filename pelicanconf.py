1e62cf2511ff21c3dfeae07e8ffd10f1c21a1843
s40523130 <40523130@gm.nfu.edu.tw>
2017-04-23 14:34:27 +0800

    blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'KMOL'
-SITENAME = '2016Fall 課程網誌 (虎尾科大MDE)'
+SITENAME = '2017 SPRING 網際內容管理 (虎尾科大MDE)'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
